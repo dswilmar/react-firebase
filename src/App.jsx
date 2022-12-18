7424a5a704d3133332c2e5b8ca848a968a25d5f0
Wilmar dos Santos <dswilmar@gmail.com>
2022-12-18 08:15:50 -0300

    Login/logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { auth, db } from "./firebaseConnection";
 import { doc, collection, addDoc, getDoc, getDocs, updateDoc, deleteDoc, onSnapshot } from "firebase/firestore";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 function App() {
 
@@ -11,7 +11,10 @@ function App() {
   const [posts, setPosts] = useState([]);
 
   const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('')
+  const [senha, setSenha] = useState('');
+
+  const [user, setUser] = useState(false);
+  const [userDetail, setUserDetail] = useState({});
   
   useEffect(() => {
     async function loadPosts() {
@@ -113,15 +116,52 @@ function App() {
     });
   }
 
+  async function logarUsuario() {
+    await signInWithEmailAndPassword(auth, email, senha).then((value) => {
+      alert('Usuário logado com sucesso!');
+      setUserDetail({
+        uid: value.user.id,
+        email: value.user.email
+      });
+      setUser(true);
+      setEmail('');
+      setSenha('');
+      console.log(value.user);
+    }).catch((error) => {
+      alert('Ocorreu um erro ao efetuar o login!');
+      console.log(error);
+    })
+  }
+
+  async function fazerLogout() {
+    await signOut(auth).then(() => {
+      alert('Logout feito com sucesso!');
+      setUser(false);
+      setUserDetail({});
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
   return (
     <div>      
       <h1>ReactJS + Firebase</h1>
+      { user && (
+        <div>
+          <strong>Seja bem vindo! Você está logado!</strong><br />
+          <span>Email: {userDetail.email}</span>
+          <button onClick={fazerLogout}>Sair da conta</button>
+          <br />
+          <br />
+        </div>
+      )}
       <div className="container">
         <label>E-mail</label>
         <input type="email" value={email} placeholder="Digite o e-mail" onChange={(e) => setEmail(e.target.value)} />
         <label>Senha</label>
         <input type="password" value={senha} placeholder="Digite a senha" onChange={(e) => setSenha(e.target.value)} />
         <button onClick={novoUsuario}>Cadastrar</button>
+        <button onClick={logarUsuario}>Fazer login</button>
       </div>
       <hr />
       <div className="container">
